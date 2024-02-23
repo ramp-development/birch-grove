f473920a01750f13b47bd1507a619c9cd8a7fe3f
g4knr <85592362+g4knr@users.noreply.github.com>
2024-02-23 12:12:14 +0000

    Carousels now fit multiple layouts

--- src/pages/houses.ts
+++ src/pages/houses.ts
@@ -1,22 +1,39 @@
 import Splide from '@splidejs/splide';
 
+import { queryElement } from '$utils/queryElement';
 import { queryElements } from '$utils/queryElements';
 
 export const houses = () => {
   console.log('houses');
 
-  const carousels = queryElements('.splide');
+  const attr = 'data-carousel';
+  const carousels = queryElements(`[${attr}="component"]`);
+
   carousels.forEach((carousel) => {
+    const { parentElement } = carousel;
+    if (parentElement) {
+      const parent = queryElement(
+        `.layout-grid:not(.w-condition-invisible) [${attr}="parent"]`,
+        parentElement
+      );
+
+      if (parent) parent.appendChild(carousel);
+    }
+
     const { type } = carousel.dataset;
     const perPage = type === 'Full' ? 1 : type === 'Halves' ? 2 : type === 'Thirds' ? 3 : 1;
 
+    const slides = queryElements('.splide__slide', carousel);
+    const numberOfSlides = slides.length;
+
     const slider = new Splide(carousel, {
-      type: 'loop',
+      type: numberOfSlides > 1 ? 'loop' : 'slide',
       autoplay: true,
       perPage,
       perMove: 1,
       gap: 'var(--gap)',
       pagination: false,
+      arrows: numberOfSlides > 1 ? true : false,
       breakpoints: {
         767: {
           perPage: 1,
